9e102bca778ebdc122ca5f4e26293d803689f310
Tautvydas Navickas <taut.navickas@gmail.com>
2023-02-15 17:51:51 +0200

    Section 16 - applying regular expressions

--- src/main/java/com/example/demo/Entity/Customer.java
+++ src/main/java/com/example/demo/Entity/Customer.java
@@ -1,9 +1,6 @@
 package com.example.demo.Entity;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 
 public class Customer {
 
@@ -14,6 +11,17 @@ public class Customer {
     @Min(value = 0, message = "must be greater than or equal to zero")
     @Max(value = 10, message = "must be greater than or equal to 10")
     private int freePasses;
+
+    @Pattern(regexp="^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
+    private String postalCode;
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
 
     public int getFreePasses() {
         return freePasses;
--- src/main/webapp/WEB-INF/view/customer-form.jsp
+++ src/main/webapp/WEB-INF/view/customer-form.jsp
@@ -23,6 +23,9 @@ Fill out the form. Asterisk (*) means required.
     Free passes: <form:input path="freePasses"/>
     <form:errors path="freePasses" cssClass="error"/>
     <br><br>
+    Postal Code: <form:input path="postalCode"/>
+    <form:errors path="postalCode" cssClass="error"/>
+    <br><br>
     <input type="submit" value="Submit"/>
 </form:form>
 </body>
