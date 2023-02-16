ec33ea8e48b6d0cbbbdb59c6e2abd3838acecd5c
Tautvydas Navickas <taut.navickas@gmail.com>
2023-02-17 00:05:14 +0200

    Section 21 Hibernate DELETE feature was added

---
+++ src/main/java/com/example/demo/Demo/DeleteStudentDemo.java
@@ -0,0 +1,31 @@
+package com.example.demo.Demo;
+
+import com.example.demo.Entity.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class DeleteStudentDemo {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+
+        Session session = factory.getCurrentSession();
+
+        try {
+            int studentId = 1;
+            session.beginTransaction();
+//            Student student1 = session.get(Student.class, studentId);
+//            session.delete(student1);
+            session.createQuery("delete from Student where id=2").executeUpdate();
+            session.getTransaction().commit();
+            System.out.println("Done");
+        } finally {
+            factory.close();
+        }
+
+
+    }
+}
