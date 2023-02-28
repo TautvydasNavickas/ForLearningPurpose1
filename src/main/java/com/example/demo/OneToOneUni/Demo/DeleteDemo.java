c3ace8c00844088dd94acff71f0ee72b449f44d3
Tautvydas Navickas <taut.navickas@gmail.com>
2023-02-28 22:43:07 +0200

    Delete feature added

---
+++ src/main/java/com/example/demo/OneToOneUni/Demo/DeleteDemo.java
@@ -0,0 +1,38 @@
+package com.example.demo.OneToOneUni.Demo;
+
+import com.example.demo.OneToOneUni.Entity.Instructor;
+import com.example.demo.OneToOneUni.Entity.InstructorDetail;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class DeleteDemo {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .buildSessionFactory();
+
+
+
+        try {
+            Session session = factory.getCurrentSession();
+            session.beginTransaction();
+
+            int id = 3;
+            Instructor instructor = session.get(Instructor.class, id);
+            if (instructor != null) {
+                session.delete(instructor);
+            }
+
+            session.getTransaction().commit();
+
+            System.out.println("Done");
+        } finally {
+            factory.close();
+        }
+
+
+    }
+}
