32effbfc28349017992f3ac7150a458e39f54e9b
Tautvydas Navickas <taut.navickas@gmail.com>
2023-02-28 23:26:32 +0200

    Bi-directional cascade delete

---
+++ src/main/java/com/example/demo/OneToOneUni/Demo/BiDirectional/DeleteInstructorDetailDemo.java
@@ -0,0 +1,43 @@
+package com.example.demo.OneToOneUni.Demo.BiDirectional;
+
+import com.example.demo.OneToOneUni.Entity.Instructor;
+import com.example.demo.OneToOneUni.Entity.InstructorDetail;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class DeleteInstructorDetailDemo {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .buildSessionFactory();
+
+            Session session = factory.getCurrentSession();
+
+        try {
+
+            session.beginTransaction();
+            int id = 1;
+            InstructorDetail instructorDetail = session.get(InstructorDetail.class, id);
+            System.out.println("Instruction detail" + instructorDetail);
+            System.out.println("Instructor" + instructorDetail.getInstructor());
+
+            session.delete(instructorDetail);
+
+            session.getTransaction().commit();
+
+            System.out.println("Done");
+        }
+        catch (Exception exception) {
+            exception.printStackTrace();
+        }
+        finally {
+            session.close();
+            factory.close();
+        }
+
+
+    }
+}
