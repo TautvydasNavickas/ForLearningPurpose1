1f3b77574074f87d332f34cdb37266779234e0fc
Tautvydas Navickas <taut.navickas@gmail.com>
2023-02-28 22:00:42 +0200

    Prep work

---
+++ src/main/java/com/example/demo/OneToOneUni/Entity/Instructor.java
@@ -0,0 +1,83 @@
+package com.example.demo.OneToOneUni.Entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "instructor")
+public class Instructor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id ;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String LastName;
+    @Column(name = "email")
+    private String email;
+    @OneToOne(cascade =CascadeType.ALL)
+    @JoinColumn(name = "instructor_detail_id")
+    private InstructorDetail instructorDetail;
+
+    public Instructor(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        LastName = lastName;
+        this.email = email;
+    }
+
+    public Instructor() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public InstructorDetail getInstructorDetail() {
+        return instructorDetail;
+    }
+
+    public void setInstructorDetail(InstructorDetail instructorDetail) {
+        this.instructorDetail = instructorDetail;
+    }
+
+    @Override
+    public String toString() {
+        return "Instructor{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", LastName='" + LastName + '\'' +
+                ", email='" + email + '\'' +
+                ", instructorDetail=" + instructorDetail +
+                '}';
+    }
+}
