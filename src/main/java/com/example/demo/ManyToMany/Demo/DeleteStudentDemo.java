5bf4c607f632ed4176136f4f556b2605c5d519de
Tautvydas Navickas <taut.navickas@gmail.com>
2023-03-02 23:22:43 +0200

    Deleting course and student feature was added

---
+++ src/main/java/com/example/demo/ManyToMany/Demo/DeleteStudentDemo.java
@@ -0,0 +1,39 @@
+package com.example.demo.ManyToMany.Demo;
+
+
+import com.example.demo.ManyToMany.Entity.*;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class DeleteStudentDemo {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .addAnnotatedClass(Course.class)
+                .addAnnotatedClass(Review.class)
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+
+        Session session = factory.getCurrentSession();
+
+        try {
+            session.beginTransaction();
+
+            int id = 6;
+            Student student = session.get(Student.class, id);
+            session.delete(student);
+
+            session.getTransaction().commit();
+
+            System.out.println("Done");
+        } finally {
+            session.close();
+            factory.close();
+        }
+
+
+    }
+}
