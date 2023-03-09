4941607e214aa0b9a682dd6e45f70e54817d886e
Tautvydas Navickas <taut.navickas@gmail.com>
2023-03-09 22:26:54 +0200

    Test database connection

---
+++ src/main/java/com/example/demo/Servlet.java
@@ -0,0 +1,46 @@
+package com.example.demo;
+
+import javax.servlet.*;
+
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+@WebServlet(name = "Servlet...", value = "/Servlet...")
+public class Servlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String user = "springstudent";
+        String pass = "springstudent";
+        String jdbcUrl = "jdbc:mysql://127.0.0.1:3306/?user=springstudent";
+        String driver = "com.mysql.jdbc.Driver";
+
+        try{
+            PrintWriter out = response.getWriter();
+
+            out.println("Connecting to database: " + jdbcUrl);
+
+            Class.forName(driver);
+
+            Connection myConn = DriverManager.getConnection(jdbcUrl, user, pass);
+
+            out.println("SUCCESS!!!");
+
+            myConn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new ServletException(e);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
